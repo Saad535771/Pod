7d20305cc4565ff8ee2029e7e19142ff616a2856
Saad Saleem <112403791+Saad535771@users.noreply.github.com>
2024-02-15 16:39:50 +0500

    initial commit

---
+++ src/views/base/tables/Tables.js
@@ -0,0 +1,70 @@
+/* eslint-disable prettier/prettier */
+import React from 'react'
+import {
+  CCard,
+  CCardBody,
+  CCardHeader,
+  CCol,
+  CRow,
+  CTable,
+  CTableBody,
+  CTableCaption,
+  CTableDataCell,
+  CTableHead,
+  CTableHeaderCell,
+  CTableRow,
+} from '@coreui/react'
+import { DocsExample } from 'src/components'
+
+const Tables = () => {
+  return (
+    <CRow>
+      <CCol xs={12}>
+        <CCard className="mb-4">
+          <CCardHeader>
+          <strong>Podcaster</strong> <small>Login</small>
+          </CCardHeader>
+          <CCardBody>
+            <p className="text-medium-emphasis small">
+              Using the most basic table CoreUI, here&#39;s how <code>&lt;CTable&gt;</code>-based
+              tables look in CoreUI.
+            </p>
+            <DocsExample href="components/table">
+              <CTable>
+                <CTableHead>
+                  <CTableRow>
+                    <CTableHeaderCell scope="col">#</CTableHeaderCell>
+                    <CTableHeaderCell scope="col">Class</CTableHeaderCell>
+                    <CTableHeaderCell scope="col">Heading</CTableHeaderCell>
+                    <CTableHeaderCell scope="col">Heading</CTableHeaderCell>
+                  </CTableRow>
+                </CTableHead>
+                <CTableBody>
+                  <CTableRow>
+                    <CTableHeaderCell scope="row">1</CTableHeaderCell>
+                    <CTableDataCell>Mark</CTableDataCell>
+                    <CTableDataCell>Otto</CTableDataCell>
+                    <CTableDataCell>@mdo</CTableDataCell>
+                  </CTableRow>
+                  <CTableRow>
+                    <CTableHeaderCell scope="row">2</CTableHeaderCell>
+                    <CTableDataCell>Jacob</CTableDataCell>
+                    <CTableDataCell>Thornton</CTableDataCell>
+                    <CTableDataCell>@fat</CTableDataCell>
+                  </CTableRow>
+                  <CTableRow>
+                    <CTableHeaderCell scope="row">3</CTableHeaderCell>
+                    <CTableDataCell colSpan="2">Larry the Bird</CTableDataCell>
+                    <CTableDataCell>@twitter</CTableDataCell>
+                  </CTableRow>
+                </CTableBody>
+              </CTable>
+            </DocsExample>
+          </CCardBody>
+        </CCard>
+      </CCol>
+    </CRow>
+  )
+}
+
+export default Tables
---
+++ src/views/pages/login/Login.js
@@ -0,0 +1,128 @@
+/* eslint-disable prettier/prettier */
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import {
+  CButton,
+  CCard,
+  CCardBody,
+  CCardGroup,
+  CCol,
+  CContainer,
+  CForm,
+  CFormInput,
+  CInputGroup,
+  CInputGroupText,
+  CRow,
+} from '@coreui/react'
+import CIcon from '@coreui/icons-react'
+import { cilLockLocked, cilUser } from '@coreui/icons'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+const Login = () => {
+  const [formData,setFormData]=useState({email:'',password:''});
+  const navigation=useNavigate();
+
+  // form Data value store
+  const handleFormData=(event)=>{
+    const {value,name}=event.target;
+    setFormData((prevUser) => ({
+      ...prevUser,
+      [name]: value
+    }));
+  };
+
+//API Method
+const show=async()=>{
+
+  var requestbody={
+    "email": formData.email,
+    "password": formData.password
+}
+
+try{
+
+  const response=await axios.post('http://localhost:7000/api/users/login',requestbody,{
+    headers:{
+
+    }
+  });
+  if(response.message === 'Wrong email/password'){
+    alert(response.message);
+  }else{
+    navigation('/dashboard')
+  }
+
+}catch(error){
+  console.error();
+  alert(error.response.data.message);
+}
+
+}
+
+  return (
+    <div className="bg-light min-vh-100 d-flex flex-row align-items-center">
+      <CContainer>
+        <CRow className="justify-content-center">
+          <CCol md={8}>
+            <CCardGroup>
+              <CCard className="p-4">
+                <CCardBody>
+                  <CForm>
+                    <h1>Login</h1>
+                    <p className="text-medium-emphasis">Sign In to your account</p>
+                    <CInputGroup className="mb-3">
+                      <CInputGroupText>
+                        <CIcon icon={cilUser} />
+                      </CInputGroupText>
+                      <CFormInput placeholder="email" name="email" onChange={handleFormData} autoComplete="username" />
+                    </CInputGroup>
+                    <CInputGroup className="mb-4">
+                      <CInputGroupText>
+                        <CIcon icon={cilLockLocked} />
+                      </CInputGroupText>
+                      <CFormInput
+                        type="password"
+                        name='password'
+                        onChange={handleFormData}
+                        placeholder="Password"
+                        autoComplete="current-password"
+                      />
+                    </CInputGroup>
+                    <CRow>
+                      <CCol xs={6}>
+                        <CButton color="primary" className="px-4" onClick={show}>
+                          Login
+                        </CButton>
+                      </CCol>
+                      <CCol xs={6} className="text-right">
+                        <CButton color="link" className="px-0">
+                          Forgot password?
+                        </CButton>
+                      </CCol>
+                    </CRow>
+                  </CForm>
+                </CCardBody>
+              </CCard>
+              <CCard className="text-white bg-primary py-5" style={{ width: '100%' }}>
+                <CCardBody className="text-center">
+                  <div>
+                    <h2>Welcome to PodMatched!</h2>
+      <p>Discover your perfect podcast matches and start enjoying personalized content.</p>
+      <p>Please sign in to access your account and explore the world of curated podcasts.</p>
+                    <Link to="/register">
+                      <CButton color="primary" className="mt-3" active tabIndex={-1}>
+                        Register Now!
+                      </CButton>
+                    </Link>
+                  </div>
+                </CCardBody>
+              </CCard>
+            </CCardGroup>
+          </CCol>
+        </CRow>
+      </CContainer>
+    </div>
+  )
+}
+
+export default Login
